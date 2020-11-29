164032bf0dd1be796d5b7051ff069d2666902a40
Evgeny <eodinzov94@gmail.com>
2020-11-29 17:39:54 +0200

    New templates added

--- EZMart/Basic/views.py
+++ EZMart/Basic/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    
+
+
+
+    return render(request, 'register.html')
--- EZMart/EZMart/urls.py
+++ EZMart/EZMart/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('',include('Basic.urls')),
     path('admin/', admin.site.urls),
+    path('accouts/',include('accounts.urls')),
     
 ]
