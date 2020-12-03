c9d1ba2a767c380d610c070f948f37a90e94801b
Ron Vayner <ronv.sce@gmail.com>
2020-12-03 22:56:41 +0200

    deleted mistakes

--- DB/pgAdmin 4/web/config_distro.py
+++
@@ -1,8 +0,0 @@
-SERVER_MODE = False
-HELP_PATH = '../../../docs/en_US/html/'
-UPGRADE_CHECK_KEY = 'edb-pgadmin4'
-DEFAULT_BINARY_PATHS = {
-"pg":   "C:\\EZMart\\DB\\bin",
-"ppas": ""
-}
-                    
