1c1f00e1714cb3bb853cbd96f89dc499b4d866f4
Ron Vayner <ronv.sce@gmail.com>
2020-12-05 20:49:47 +0200

    Moved hackathon related snapshot to designated branch

---
+++ EZMart/accounts/models.py
@@ -0,0 +1,103 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+from django.core.validators import RegexValidator
+
+USERNAME_REGEX = '^[a-zA-Z0-9.+-]*$'
+
+class MyAccManager(BaseUserManager):
+    def create_user(self,email,first_name,last_name,address,phone,username,password=None,businessNum='None',businessType='None',store_ID=None,is_business=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have an username")
+        user = self.model(
+            email=self.normalize_email(email),
+            username = username,
+            first_name=first_name,
+            last_name=last_name,
+            address=address,
+            phone=phone,
+            businessNum='None',
+            businessType='None',
+            store_ID=-1,
+            is_business=False,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self,username,email,password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username = username,
+            first_name='Admin',
+            last_name='Admin',
+            address='None',
+            phone='None',
+            password = password,
+            businessNum='None',
+            businessType='None',
+            store_ID=-1,
+            is_business=False,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True 
+        user.save(using=self._db)
+        return user
+    def create_business_user(self,email,first_name,last_name,address,phone,username,businessNum,businessType,store_ID,is_business=True,password=None,):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username = username,
+            first_name=first_name,
+            last_name=last_name,
+            address=address,
+            phone=phone,
+            password = password,
+            businessNum=businessNum,
+            businessType=businessType,
+            store_ID=store_ID,
+            is_business=True,
+        )
+        user.is_business=True
+        user.businessType=businessType
+        user.businessNum=businessNum
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    username                    = models.CharField(
+        max_length=300,
+        validators = [
+            RegexValidator(regex = USERNAME_REGEX,
+            message='Username must be alphanumeric or contain numbers',
+            code='invalid_username'
+            )],
+         unique=True)
+    email                    = models.EmailField(max_length=255,unique=True,verbose_name="email address")
+    data_joined              = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login               = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin                 = models.BooleanField(default=False)
+    is_active                = models.BooleanField(default=True)
+    is_staff                 = models.BooleanField(default=False)
+    is_superuser             = models.BooleanField(default=False)
+    first_name               = models.CharField(max_length=30, default=None)
+    last_name                = models.CharField(max_length=30, default=None)
+    address                  = models.CharField(max_length=150, default=None)
+    phone                    = models.CharField(max_length=14, default=None)
+    businessNum              = models.CharField(max_length=14, default=None)
+    businessType             = models.CharField(max_length=30, default=None)
+    store_ID                 = models.IntegerField(default=-1)
+    is_business              = models.BooleanField(default=False)    
+
+    objects = MyAccManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+    def has_perm(self,perm,obj=None):
+        return self.is_admin
+    def has_module_perms(self,app_label):
+        return True
+    
---
+++ EZMart/accounts/views.py
@@ -0,0 +1,164 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import auth
+from django.contrib import messages
+from .models import Account
+
+
+def registercustomer(request):
+    if request.method == 'POST' :
+
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        address = request.POST['address']
+        phone = request.POST['phone']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if len(first_name)<2:
+            messages.info(request,'First name to short')
+            return redirect('registercustomer')
+        if len(last_name)<2:
+            messages.info(request,'First name to short')
+            return redirect('registercustomer')
+        if len(username)<6:
+            messages.info(request,'Username to short, should be atleast 6 symbols')
+            return redirect('registercustomer')
+        if len(address)<6:
+            messages.info(request,'Address to short')
+            return redirect('registercustomer')
+        if len(phone)<8 :
+            messages.info(request,'Phone number to short')
+            return redirect('registercustomer')
+        if len(password1)<6 :
+            messages.info(request,'Password to short, should be atleast 6 symbols')
+            return redirect('registercustomer')    
+        if password1==password2:
+            if Account.objects.filter(username=username).exists():
+                messages.info(request,'Username is already taken')
+                return redirect('registercustomer')
+            elif Account.objects.filter(email=email).exists():
+                messages.info(request,'Email already exists')
+                return redirect('registercustomer')
+            else:
+                user = Account.objects.create_user(
+                email=email, 
+                first_name=first_name,
+                last_name=last_name,
+                address=address,
+                phone=phone,
+                username=username,
+                password=password1,
+                businessNum='None',
+                businessType='None',
+                store_ID=-1,
+                is_business=False
+            )
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request,'Passwords do not match')
+            return redirect('registercustomer')
+        
+        
+    else:
+        return render(request, 'registercustomer.html')
+
+
+    return render(request, 'registercustomer.html') 
+        
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid credentials')
+            return redirect('login')
+
+
+    else:
+        return render(request,'Login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def Cart(request):
+    return render(request,'Cart.html')
+
+def Payment(request):
+    return render(request,'Payment.html')
+
+def registerbusiness(request):
+    if request.method == 'POST' :
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        address = request.POST['address']
+        phone = request.POST['phone']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        businesstype = request.POST['businesstype']
+        businessnum = request.POST['businessnum']
+
+        if len(businessnum)<6:
+            messages.info(request,'Business number to short')
+            return redirect('registerbusiness')  
+        if len(first_name)<2:
+            messages.info(request,'First name to short')
+            return redirect('registerbusiness')
+        if len(last_name)<2:
+            messages.info(request,'First name to short')
+            return redirect('registerbusiness')
+        if len(username)<6:
+            messages.info(request,'Username to short, should be atleast 6 symbols')
+            return redirect('registerbusiness')
+        if len(address)<6:
+            messages.info(request,'Address to short')
+            return redirect('registerbusiness')
+        if len(phone)<8 :
+            messages.info(request,'Phone number to short')
+            return redirect('registerbusiness')
+        if len(password1)<6 :
+            messages.info(request,'Password to short, should be atleast 6 symbols')
+            return redirect('registerbusiness')    
+        if password1==password2:
+            if Account.objects.filter(username=username).exists():
+                messages.info(request,'Username is already taken')
+                return redirect('registerbusiness')
+            elif Account.objects.filter(email=email).exists():
+                messages.info(request,'Email already exists')
+                return redirect('registerbusiness')
+            else:
+                user = Account.objects.create_business_user(
+                email=email, 
+                first_name=first_name,
+                last_name=last_name,
+                address=address,
+                phone=phone,
+                username=username,
+                password=password1,
+                businessNum=businessnum,
+                businessType=businesstype,
+                store_ID=-1,
+                is_business=True
+            )
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request,'Passwords do not match')
+            return redirect('registerbusiness')
+        
+        
+    else:
+        return render(request, 'registerbusiness.html')
+
+
+    return render(request, 'registerbusiness.html') 
