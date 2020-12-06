0639422acdf702736040618b19a270855a9280d2
Tal O <taloh13@gmail.com>
2020-12-06 17:50:43 +0200

    businessProfile html/css , functionality - views,urls

---
+++ EZMart/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('registercustomer',views.registercustomer, name='registercustomer'),
+   path('cart', views.Cart, name='cart'),
+   path('payment', views.Payment, name='payment'),
+   path('login', views.Login, name='login'),
+	path("logout",views.logout, name="logout"),
+   path('registerbusiness',views.registerbusiness, name='registerbusiness'),
+   path('userProfile',views.userProfile, name='userProfile'),
+   path('businessProfile',views.businessProfile, name='businessProfile'),
+]
