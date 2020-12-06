dc6b42612606ed83fda196550e434b168d769093
Ron Vayner <ronv.sce@gmail.com>
2020-12-06 17:57:31 +0200

    test

--- EZMart/Shop/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,redirect
-from django.contrib import messages
-from .models import Store
-
-def manage_products(self):
-    def get_products():
-            return self.products
-    def addProduct(Product):
-            self.products.add(Product)
